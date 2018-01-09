443ad2609c4b624a2e26c0d8fe2b1358309ca5b3
Joseph Hughes <jjhughes57@gmail.com>
2018-01-09 17:14:03 -0500

    add https

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ set -eo pipefail
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 # Build cartographer
 ## Get dependencies
+apt-get update
+apt-get install apt-transport-https
 echo "deb https://${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD}@sixriver.jfrog.io/sixriver/debian xenial main" >> /etc/apt/sources.list
 apt-get update
 apt-get install -y curl python-wstool python-rosdep ninja-build
