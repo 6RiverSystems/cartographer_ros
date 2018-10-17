f4bd0bee6079a6ebc5fc3431b927b052db74fc35
Dan Winkler <dan@6river.com>
2018-10-17 17:17:16 -0400

    fix: Attempt to fix install script.

--- install.sh
+++ install.sh
@@ -5,35 +5,7 @@ source "/opt/ros/$ROS_DISTRO/setup.bash"
 # Build cartographer
 ## Get dependencies
 apt-get update
-apt-get install apt-transport-https
-
-# Install CMake 3.2 for Ubuntu Trusty and Debian Jessie.
-apt-get install lsb-release -y
-if [[ "$(lsb_release -sc)" = "trusty" ]]
-then
-  apt-get install cmake3 -y
-elif [[ "$(lsb_release -sc)" = "jessie" ]]
-then
-  sh -c "echo 'deb http://ftp.debian.org/debian jessie-backports main' >> /etc/apt/sources.list"
-  apt-get update
-  apt-get -t jessie-backports install cmake -y
-else
-  apt-get install cmake -y
-fi
-
-apt-get install -y \
-    g++ \
-    git \
-    google-mock \
-    libboost-all-dev \
-    libcairo2-dev \
-    libeigen3-dev \
-    libgflags-dev \
-    libgoogle-glog-dev \
-    liblua5.2-dev \
-    libsuitesparse-dev \
-    ninja-build \
-    python-sphinx
+apt-get install -y apt-transport-https python-wstool python-rosdep ninja-build
 
 ARCH=$(dpkg --print-architecture)
 # Make the directory
@@ -46,6 +18,7 @@ VERSION="$(cat .version)"
 
 # Uncomment below to test locally
 #VERSION='fix Jenkins'
+
 # Init workspace
 cd /opt/cartographer
 wstool init src
