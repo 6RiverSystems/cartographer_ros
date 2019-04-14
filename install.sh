d2d89110cf83dda312b76dfbd5b9a400535c31cb
Dimitri Nikiopoulos <dnikitop@gmail.com>
2019-04-14 19:11:37 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ source "/opt/ros/$ROS_DISTRO/setup.bash"
 # Build cartographer
 ## Get dependencies
 
-cat /docker-deps/artifactory_key.pub | apt-key add - && \
+cat ${WORKSPACE}/docker-deps/artifactory_key.pub | apt-key add - && \
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116 && \
     echo "deb https://${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD}@sixriver.jfrog.io/sixriver/debian xenial main" >> /etc/apt/sources.list && \
     echo "deb https://${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD}@sixriver.jfrog.io/sixriver/ros-ubuntu xenial main" >> /etc/apt/sources.list
